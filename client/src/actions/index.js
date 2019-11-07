c5d5e88c0781a74aa5b5176ab45f0a7ce719f731
Alejandro Munoz <alejandro.munoz.casarez@gmail.com>
2019-11-06 22:59:14 -0800

    pass userId when signIn

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,15 +1,14 @@
 import { SIGN_IN, SIGN_OUT } from './types';
 
-export const signIn = () => {
+export const signIn = (userId) => {
     return {
-        type: SIGN_IN
+        type: SIGN_IN,
+        payload: userId,
     };
 };
 
 export const signOut = (userId) => {
     return {
-        type: SIGN_OUT,
-        payload: userId,
-
+        type: SIGN_OUT
     };
 };
